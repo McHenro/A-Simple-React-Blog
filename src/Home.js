d16e6c71842eba5e5325f6caf1af7ece23410e65
Henry <adolphosita@yahoo.com>
2022-10-26 18:36:44 +0100

    using async await to fetch data inside uesEffect

--- src/Home.js
+++ src/Home.js
@@ -2,34 +2,42 @@ import { useState } from "react";
 import { useEffect } from "react";
 import BlogList from "./BlogList";
 // import React from 'react'
-import axios from 'axios';
+// import axios from 'axios';
 
 
-const Home= () => {
-    const [blogs, setBlogs] = useState(null);
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
 
-    useEffect(()=>{
-      axios.get("http://localhost:8000/blogs")
-      .then(res=>{
-        console.log(res)
-        setBlogs(res.data);
-      })
-      .catch(err=>{
-        console.log(err)
-      }) 
-    }, [])
+  useEffect(() => {
+    async function fetchData(){
+      await fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json()
+        })
+        .then((data)=>{
+          setBlogs(data);
+        });
+      } 
+      fetchData();  
+      
+  }, []);
 
-    const handleDelete = (id) => {
-      const newBlog = blogs.filter((blog)=> blog.id !== id)
-      setBlogs(newBlog);
+  const handleDelete = (id) => {
+    const newBlog = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlog);
+  };
 
-    }
- 
-    return (
-      <div className="home">
-        {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}
-      </div>
-    );
-}
+  return (
+    <div className="home">
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title="All Blogs!"
+          handleDelete={handleDelete}
+        />
+      )}
+    </div>
+  );
+};
  
 export default Home;
