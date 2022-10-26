35ae8f25ea4e986d8b8ebb2938b58e14ebf1b4a4
Henry <adolphosita@yahoo.com>
2022-10-26 17:50:49 +0100

    using axios inside useEffect to fetch data

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,23 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import BlogList from "./BlogList";
+// import React from 'react'
+import axios from 'axios';
+
 
 const Home= () => {
-    const [blogs, setBlogs] = useState([
-      {
-        title: "My new website",
-        body: "lorem ipsum...",
-        author: "mario",
-        id: 1,
-      },
-      {
-        title: "Welcome party!",
-        body: "lorem ipsum...",
-        author: "yoshi",
-        id: 2,
-      },
-      {
-        title: "Web dev top tips",
-        body: "lorem ipsum...",
-        author: "mario",
-        id: 3,
-      },
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const [name, setName] = useState('Henry')
     useEffect(()=>{
-      console.log("run useEffect")
-    }, [name])
+      axios.get("http://localhost:8000/blogs")
+      .then(res=>{
+        console.log(res)
+        setBlogs(res.data);
+      })
+      .catch(err=>{
+        console.log(err)
+      }) 
+    }, [])
 
     const handleDelete = (id) => {
       const newBlog = blogs.filter((blog)=> blog.id !== id)
@@ -37,13 +27,7 @@ const Home= () => {
  
     return (
       <div className="home">
-        <BlogList
-          blogs={blogs}
-          title="All Blogs!"
-          handleDelete={handleDelete}
-        />
-        <button onClick={() => setName('Joshua')}>Change Name</button>
-        <p>{name}</p>
+        {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}
       </div>
     );
 }
