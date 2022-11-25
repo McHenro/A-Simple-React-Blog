8030614267c803d1106826f276232fd1c94de406
Henry <adolphosita@yahoo.com>
2022-11-25 12:01:20 +0100

    feat: add delete button

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./useFetch";
 
 
@@ -6,6 +6,15 @@ const BlogDetails = () => {
 
     const { id } = useParams();
     const { data: blog, isPending, error } = useFetch("http://localhost:8000/blogs/" + id);
+    const navigate = useNavigate()
+    const handleDelete = () =>{
+      fetch("http://localhost:8000/blogs/" + id, {
+        method: "DELETE"
+      })
+      .then(()=>{
+        navigate('/')
+      })
+    }
     return (
       <div className="blog-details">
         {isPending && <div>Loading...</div>}
@@ -15,6 +24,7 @@ const BlogDetails = () => {
             <h2>{blog.title}</h2>
             <p>Written by {blog.author}</p>
             <div>{blog.body}</div>
+            <button onClick={handleDelete}>delete</button>
           </article>
         )}
       </div>
